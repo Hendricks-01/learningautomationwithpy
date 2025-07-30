bd4d9499cdf735e9a5b4f87672ab8a5e88f889d7
Hendricks_01 <91264904+Hendricks-01@users.noreply.github.com>
2025-07-30 05:48:19 +0300

    Update pythonauto.py

--- pythonauto.py
+++ pythonauto.py
@@ -1,9 +1,9 @@
 # this program says hello and asks for my name
-print('Hello World!')
-print('What is your name?')  # ask for their name
-myName = input()  # input function waits for user input
-print('It is good to meet you, ' + myName)
-print('The length of your name is:' + str(len(myName)))  # prints the length of the name
-print('What is your age?')  # ask for their age
-myAge = input()  # input function waits for user input
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')  # tells the user their age next year
+print('hello world!')
+print('what is your name?')
+myname = input()
+print('it's good to meet you ' + myname)
+print('the length of your name is ' + str(len(myname)))
+print('what is your age?')
+myage = input()
+print('you will be ' + str(int(myage) + 1) + 'in a year.')
